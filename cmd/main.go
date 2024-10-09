32fefebd565d8d8d1e2d9cda73009776f5e538f8
AnikinSimon <s.anikin1309@gmail.com>
2024-10-09 10:54:23 +0300

    First HW Done

---
+++ cmd/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"golangProject/internal/pkg/storage"
+	"log"
+)
+
+func main() {
+	var s, err = storage.NewStorage()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s.Set("key1", "val1")
+	s.Set("key2", "123")
+
+	fmt.Println(s.Get("key1"))
+	fmt.Println(s.Get("key2"))
+	fmt.Println(s.Get("key3"))
+
+	fmt.Println(s.GetKind("key1"))
+	fmt.Println(s.GetKind("key2"))
+	fmt.Println(s.GetKind("key3"))
+
+}
