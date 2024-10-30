430a265a6f8573a7c964edb4a8a57de2a0c0970a
AnikinSimon <s.anikin1309@gmail.com>
2024-10-30 20:15:41 +0300

    Add SERVER_PORT to env

--- cmd/main.go
+++ cmd/main.go
@@ -14,10 +14,18 @@ func main() {
 	store, err := storage.NewStorage(storage.WithoutLogging())
 	if err != nil {
 		log.Panic(fmt.Errorf("InitializingError: %w", err))
+		os.Exit(1)
 	}
 
 	store.ReadStateFromFile() // считывание состояния бд из .json
-	serve := server.New(":8070", store)
+
+	server_port, ok := os.LookupEnv("SERVER_PORT")
+	if !ok {
+		log.Panic(fmt.Errorf("NoServerPort: %w", err))
+		os.Exit(1)
+	}
+	
+	serve := server.New(":"+server_port, store)
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
 
